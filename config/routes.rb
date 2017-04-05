07c561ea739ed4ad37fd149d6c8cd7d5e11914e6
hanhan <hanhan_smith@hotmail.com>
2017-04-05 21:49:32 +0800

    save

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cities
+  root 'cities#index'
+  resources :cities do
+    member do
+      post :update_temp
+    end
+  end
+
+
 end

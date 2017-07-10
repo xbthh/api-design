067edb47828d879c0299796bf4245e3be0bb2751
hanhan <hanhan_smith@hotmail.com>
2017-07-11 00:29:58 +0800

    complete find personal information

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,15 @@ class Api::V1::UsersController < ApiController
 
    before_action :authenticate_user!
 
+   def show
+     render :json => {
+       :email => current_user.email,
+       :avatar => current_user.avatar,
+       :updated_at => current_user.updated_at,
+       :created_at => current_user.created_at
+     }
+   end
+
    def update
      if current_user.update(user_params)
        render :json => { :message => "OK" }
